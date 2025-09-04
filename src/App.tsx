321319513475e030eeabd4e2a29992b5a0a947a1
RaphaelVieiraC <rapha.007.2009@gmail.com>
2025-09-04 11:21:55 -0300

    buil: Criando tela de login

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 p-6">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="login">Usuário</Label>
+                <Input
+                  id="login"
+                  type="login"
+                  placeholder="AD_NAME"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Senha</Label>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+          <CardAction className="flex-col gap-2">
+            <Button variant="outline">Registrar</Button>
+            <Button variant="link">Esqueci minha senha?</Button>
+          </CardAction>
+        </CardFooter>
+      </Card>
+    </div>
+  )
+}
+
